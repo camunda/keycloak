b4a29e6101e3c7f7e59a811cd3974c2f86af953f
leiicamundi <153937047+leiicamundi@users.noreply.github.com>
2024-03-06 14:08:22 +0100

    update request

--- .helpers/actions/drop-unused-db.sh
+++ .helpers/actions/drop-unused-db.sh
@@ -7,8 +7,8 @@ SELECT datname
 FROM pg_database
 WHERE datname NOT IN (
     SELECT datname
-    FROM pg_stat_database
-    WHERE (now() - pg_stat_database.last_stat_reset) > interval '${RETENTION_INTERVAL}'
+    FROM pg_stat_bgwriter
+    WHERE (now() - last_stats_reset) > interval '2 days'
 )
 AND has_database_privilege(datname, 'CONNECT');
 EOF
