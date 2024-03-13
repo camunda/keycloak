96886b79751921564ec64ba775cc14368f3055e1
leiicamundi <153937047+leiicamundi@users.noreply.github.com>
2024-03-13 10:42:17 +0100

    switch from python scripts to bash

---
+++ .github/scripts/utils/find_latest_keycloak.sh
@@ -0,0 +1,8 @@
+
+#!/usr/bin/env bash
+
+# Script: find_latest_keycloak.sh
+# Description: Finds the latest version of Keycloak from folders prefixed with "keycloak-" in the current directory.
+# Usage: find_latest_keycloak.sh
+
+ls -d "$(pwd)"/keycloak-* | tail -n 1 | awk -F'-' '{print $3}'
