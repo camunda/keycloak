d0f7990c511f3cd1b7b4f28b8fa375ee7de3ccc5
leiicamundi <153937047+leiicamundi@users.noreply.github.com>
2024-03-13 15:02:58 +0100

    fix sed linux

--- .pre-commit-hooks/update_readme_version.sh
+++ .pre-commit-hooks/update_readme_version.sh
@@ -2,14 +2,16 @@
 
 latest_version="$(./.github/scripts/utils/find_latest_keycloak.sh)"
 
+case "$(uname)" in
+    Darwin*) sed () { command sed -i '' "$@"; } ;;
+    *)       sed () { command sed -i "$@"; } ;;
+esac
+
 # Check if the version in the README.md file is different from the latest version
 current_version=$(grep -o "docker.io/camunda/keycloak:[0-9]\+" README.md | head -n 1 | cut -d ":" -f 2)
 if [ "${current_version}" != "${latest_version}" ]; then
-    # Replace the string in the file
-    echo "$(pwd)"
-    echo "$(ls)"
-    echo "$(tree)"
-    sed -i '' -E "s@docker.io/camunda/keycloak:[0-9]+@docker.io/camunda/keycloak:${latest_version}@g" README.md
+    sed -E "s@docker.io/camunda/keycloak:[0-9]+@docker.io/camunda/keycloak:${latest_version}@g" README.md
+
     echo "The docker image version in the README.md has been updated from docker.io/camunda/keycloak:${current_version} to docker.io/camunda/keycloak:${latest_version}" >&2
     exit 1
 fi
