17214bf6e111e4f13ba09a72c4c5aa323b4efd50
leiicamundi <153937047+leiicamundi@users.noreply.github.com>
2024-03-21 17:32:01 +0100

    update config check

--- .github/scripts/build-check/main.py
+++ .github/scripts/build-check/main.py
@@ -5,7 +5,3 @@ print('Checking that the plugins were installed properly')
 config = os.environ.get('CONFIG')
 print("Checking plugin included: aws-wrapper")
 assert 'aws-advanced-jdbc-wrapper' in config
-print("Checking plugin included: sts")
-assert 'sts' in config
-print("Checking plugin included: apache-client")
-assert 'apache-client' in config
