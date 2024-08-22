a47cb52504c71381556df27060f279ba62ed6921
Leo J. <153937047+leiicamundi@users.noreply.github.com>
2024-08-22 15:23:54 +0200

    fix tests

--- .github/scripts/build-check/main.py
+++ .github/scripts/build-check/main.py
@@ -6,10 +6,10 @@ config = os.environ.get('CONFIG')
 print("Checking plugin included: aws-wrapper")
 assert 'aws-advanced-jdbc-wrapper' in config
 
-# sts only in old versions
+# sts and apache-client only in old versions
 if 'kc.version =  24.' in config or 'kc.version =  23.' in config:
   print("Checking plugin included: sts")
   assert 'sts' in config
 
-print("Checking plugin included: apache-client")
-assert 'apache-client' in config
+  print("Checking plugin included: apache-client")
+  assert 'apache-client' in config
