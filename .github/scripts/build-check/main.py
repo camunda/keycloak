e851c093514db32236fda483504643d227e11f88
Leo J. <153937047+leiicamundi@users.noreply.github.com>
2024-08-23 16:57:08 +0200

    try with newer jdbc

--- .github/scripts/build-check/main.py
+++ .github/scripts/build-check/main.py
@@ -5,11 +5,7 @@ print('Checking that the plugins were installed properly')
 config = os.environ.get('CONFIG')
 print("Checking plugin included: aws-wrapper")
 assert 'aws-advanced-jdbc-wrapper' in config
-
-# sts and apache-client only in old versions
-if 'kc.version =  24.' in config or 'kc.version =  23.' in config:
-  print("Checking plugin included: sts")
-  assert 'sts' in config
-
-  print("Checking plugin included: apache-client")
-  assert 'apache-client' in config
+print("Checking plugin included: sts")
+assert 'sts' in config
+print("Checking plugin included: apache-client")
+assert 'apache-client' in config
